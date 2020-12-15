46f0df40bb51f1851dfb178e6d917a2f0e27e6dd
everson-souza <everson@alunos.utfpr.edu.br>
2020-12-15 10:52:48 -0300

    Dictionary fix

--- mars/dictionary.py
+++ mars/dictionary.py
@@ -1,6 +1,6 @@
 # Dicionário de palavras em português e sinais
 
 words = {
-    "rola": (0, 0, 180, 0, 0),
-    "pau": (0, 0, 180, 0, 0),
+    "rola": (0, 0, 180, 0, 0, 0)
+    "pau": (0, 0, 180, 0, 0, 0)
 }
