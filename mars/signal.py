be7e309e4d32ad86ce699ed75c3ba826093d42de
nanaulopes <joses@alunos.utfpr.edu.br>
2021-01-07 19:23:39 -0300

    Dicionario atualizado

--- mars/signal.py
+++ mars/signal.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 import threading
+import time
+from dictionary import *
 
 GPIO.setmode(GPIO.BCM)
 
@@ -9,36 +11,70 @@ finger3 = 13
 finger4 = 19
 finger5 = 26
 
-# Classe onde será implementado a movimentação dos motores
+# Classe onde sera implementado a movimentacao dos motores
 class Sinal:
     
     def __init__(self):
-        self.pwm = []
+        self.pwm = [0]*5
         self.handStart()
-        print ('lalala')
     
     def signal(self, word):
+        self.openHand()
+        time.sleep(2)
+        
         for angles in word:
-            f1 = threading.Thread(target=fingerMovement, args=(angles[0], 0))
+            print("Angles:", angles, type(angles))
+            f1 = threading.Thread(target=self.fingerMovement, args=(angles[0], 0))
             f1.start()
 
-            f2 = threading.Thread(target=fingerMovement, args=(angles[1], 1))
+            f2 = threading.Thread(target=self.fingerMovement, args=(angles[1], 1))
             f2.start()
 
-            f3 = threading.Thread(target=fingerMovement, args=(angles[2], 2))
+            f3 = threading.Thread(target=self.fingerMovement, args=(angles[2], 2))
             f3.start()
 
-            f4 = threading.Thread(target=fingerMovement, args=(angles[3], 3))
+            f4 = threading.Thread(target=self.fingerMovement, args=(angles[3], 3))
             f4.start()
 
-            f5 = threading.Thread(target=fingerMovement, args=(angles[4], 4))
+            f5 = threading.Thread(target=self.fingerMovement, args=(angles[4], 4))
             f5.start()
 
-            sleep(angles[5])
+            time.sleep(angles[5])
 
+    def closeHand(self):
+        f1 = threading.Thread(target=self.fingerMovement, args=(0, 0))
+        f1.start()
+
+        f2 = threading.Thread(target=self.fingerMovement, args=(0, 1))
+        f2.start()
+
+        f3 = threading.Thread(target=self.fingerMovement, args=(0, 2))
+        f3.start()
+
+        f4 = threading.Thread(target=self.fingerMovement, args=(0, 3))
+        f4.start()
+
+        f5 = threading.Thread(target=self.fingerMovement, args=(0, 4))
+        f5.start()
+
+    def openHand(self):
+        f1 = threading.Thread(target=self.fingerMovement, args=(220, 0))
+        f1.start()
+
+        f2 = threading.Thread(target=self.fingerMovement, args=(180, 1))
+        f2.start()
+
+        f3 = threading.Thread(target=self.fingerMovement, args=(180, 2))
+        f3.start()
+
+        f4 = threading.Thread(target=self.fingerMovement, args=(200, 3))
+        f4.start()
+
+        f5 = threading.Thread(target=self.fingerMovement, args=(180, 4))
+        f5.start()
 
     def fingerMovement(self, angle, finger):
-        print("Angulo: ", angle, " Dedo: ", finger)
+        #print("Angulo: ", angle, " Dedo: ", finger)
         duty = float(angle) / 10.0 + 2.5
         self.pwm[finger].ChangeDutyCycle(duty)
 
@@ -65,12 +101,24 @@ class Sinal:
 
 def main():
     mao = Sinal()
+    print("Iniciando a MAR2S!!!")
+    mao.openHand()
+
     while True:
-        print("Digite o angulo: ")
-        angulo = input();
-        print("Digite o dedo: ")
-        dedo = input();
-        mao.finger(angulo, dedo)
+        op = input("1- Digitar algo\n2- Entrada manual dos angulos\n")
+        opcao = int(op)
+        if(opcao == 1):
+            palavra = input("Digite algo: ")
+            print("Palavra:", palavra, type(palavra))
+            mao.signal(dictionary[palavra])
+            continue
+        if(opcao == 2):
+            print("Digite o angulo: ")
+            angulo = input();
+            print("Digite o dedo: ")
+            dedo = input();
+            mao.fingerMovement(int(angulo), int(dedo))
+            continue
 
 if __name__ == "__main__":
 	main()
